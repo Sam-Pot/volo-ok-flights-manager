ea0851325e48435a546c44b7bee9e9d4390b0a77
Salvatore Potrino <s.potrino@sinapsys.it>
2024-06-03 19:28:03 +0200

    Implemented SpringBoot basic project.

---
+++ src/main/java/com/volok/flightsManager/FlightsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.volok.flightsManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsManagerApplication.class, args);
+	}
+
+}
